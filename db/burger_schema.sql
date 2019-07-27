da3b43f64a65b197f115c7d6e12c35be3d76df1a
Johnny <johnrussoofficial@outlook.com>
2019-07-27 15:26:05 -0400

    new connection vals

--- db/burger_schema.sql
+++ db/burger_schema.sql
@@ -3,8 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-	id INT AUTO_INCREMENT NOT NULL,
+	id SERIAL PRIMARY KEY,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY (id)
 );
