cb4634a94e1adf767c598b9ec1e8c1a25ce5f1fe
Johnny <johnrussoofficial@outlook.com>
2019-07-27 14:23:27 -0400

    deployed

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
 
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASSWORD,
-  database: "burgers_db"
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: process.env.DB_PASSWORD,
+//   database: "burgers_db"
+// });
+
+var Client = require("pg");
+
+var connection = new Client({connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
+
+connection.connect();
+
 
 connection.connect(function(err) {
   if (err) {
